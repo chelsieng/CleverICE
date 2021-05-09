cf63d78059126047fdd5c05c896309d04223cd24
Duc Nguyen <duc.duke.ngn@gmail.com>
2021-05-09 12:14:08 -0400

    Extract information from response to JSON

--- backend/config.py
+++ backend/config.py
@@ -6,6 +6,6 @@ class Config(object):
     """
     Get the configurations.
     """
-    dotenv_path = '../.env'
+    dotenv_path = './.env'
     load_dotenv(dotenv_path)
     OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
--- backend/openai_request.py
+++ backend/openai_request.py
@@ -1,4 +1,4 @@
-import os
+import os, json
 import openai
 from .config import Config
 
@@ -6,7 +6,8 @@ class openAI:
     prefix = 'Here is a claim letter from a customer to an insurance company.\
             Read the letter, then answer the following questions\n\n\
             """\n'
-    answer_prefix = 'Answer:\n1.'
+    answer_prefix = 'Answer:\nAnswer-1.'
+    keywords = ['Name', 'Phone number', 'Insurance policy number', 'Email', 'Incident', 'Location of the incident', 'Requested money', 'Summary']
 
     def __init__(self):
        openai.api_key = Config.OPENAI_API_KEY
@@ -37,10 +38,12 @@ class openAI:
 
         # Append prefix and suffix to create meaningful query.
         prompt = self.prefix + prompt + '\n"""\n' + suffix + self.answer_prefix
-        print(prompt)
-        response = self.clean(self.make_request(prompt, temperature=0.3, max_tokens=250, frequency_penalty=0.16))
-        print(response)
+        response = self.clean(self.make_request(prompt, temperature=0.6, max_tokens=250, frequency_penalty=0.2)).split('Answer-')
+        print(self.extract(response))
         return response
+
+    def extract(self, response):
+        return json.dumps({self.keywords[i]: response[i][:-1] for i in range(len(self.keywords))}, indent=4)
 
     def clean(self, response):
         return response.choices[0].text
@@ -54,4 +57,25 @@ class openAI:
         response = self.clean(self.make_request(question, temperature=0.5, max_tokens=250))
         return response
 
-    
+
+
+"""
+# Test
+
+test_letter = 'Dear Sir/Madam, \
+\
+This letter is to formally request reimbursement for medical expenses for policy 135-8452-1268. I was visiting Niagara Fall last Sunday (May 2nd, 2021) where I fell and broke my wrist.\
+\
+I was treated at Toronto General Hospital, and because I was from out-of-state, I needed to pay the bill in full. The amount was $3500 for x-rays, a bone specialist and one night in the hospital.\
+\
+According to the terms of my policy, I contacted my primary healthcare physician within 24 hours of the accident. I have enclosed all the documents related to my treatment. I understand that I have a $500 deductible on my policy. I am requesting you to reimburse me $3000 in keeping with my health care coverage.\
+\
+I can be reached at 538-753-3578 or at christyang@gmail.com if you have any questions. Thank you for your quick attention to this.\
+\
+Sincerely,\
+\
+Christ'
+
+model = openAI()
+response = model.scan(test_letter)
+"""
