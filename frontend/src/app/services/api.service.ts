4a4a51174727953ddf16097daadd70524282abf9
chelsieng <chelsiengmk@gmail.com>
2021-05-09 04:46:00 -0400

    api fix

--- frontend/src/app/services/api.service.ts
+++ frontend/src/app/services/api.service.ts
@@ -1,8 +1,6 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-
-class Observable<T> {
-}
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -14,14 +12,14 @@ export class ApiService {
     this.http = http;
   }
 
-  // getRequestName(argumentName?: typedef): Observable<returnTypeDef> {
-  //   let url = '/xxxxx/';
-  //   // some operations here
-  //   return this.http.get<returnTypeDef>(url);
-  // }
-  //
-  // postRequestName(argumentName: typedef): Observable<returnTypeDef> {
-  //   const url = '/xxxxx';
-  //   return this.http.post<returnTypeDef>(url, argumentName);
-  // }
+  getRequestName(argumentName?: typedef): Observable<returnTypeDef> {
+    let url = '/xxxxx/';
+    // some operations here
+    return this.http.get<returnTypeDef>(url);
+  }
+
+  postRequestName(argumentName: typedef): Observable<returnTypeDef> {
+    const url = '/xxxxx';
+    return this.http.post<returnTypeDef>(url, argumentName);
+  }
 }
