16310dc8873be53803b556fa7f66bb2a8fe80bce
niyonx <kenkenysy@gmail.com>
2021-05-08 21:49:38 -0400

    Storage operations

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from pdftotext import async_detect_document
+from googlecloud import *
 
 app = Flask(__name__)
 
@@ -8,9 +8,18 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World from Flask!'
 
+# called directly by upload pdf
 @app.route('/pdftotext/<file>')
 def pdftotext(file):
     return async_detect_document(file)
+
+@app.route('/uploadblob/<file>')
+def uploadblob(file):
+    return upload_blob(file)
+
+@app.route('/getpdftext/<file>')
+def getpdftext(file):
+    return get_pdf_text(file)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- backend/googlecloud.py
+++ backend/googlecloud.py
@@ -10,7 +10,8 @@ os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="credentials.json"
 def async_detect_document(gcs_source_uri, gcs_destination_uri="gs://tohacks-buc/pdf_results/"):
     """OCR with PDF/TIFF as source files on GCS"""
 
-    gcs_source_uri = "gs://tohacks-buc/"+gcs_source_uri
+    gcs_destination_uri = gcs_destination_uri + gcs_source_uri
+    gcs_source_uri = "gs://tohacks-buc/" + gcs_source_uri
     # Supported mime_types are: 'application/pdf' and 'image/tiff'
     mime_type = 'application/pdf'
 
@@ -59,7 +60,7 @@ def async_detect_document(gcs_source_uri, gcs_destination_uri="gs://tohacks-buc/
     # Process the first output file from GCS.
     # Since we specified batch_size=2, the first response contains
     # the first two pages of the input file.
-    output = blob_list[1]
+    output = blob_list[0]
 
     json_string = output.download_as_string()
     response = json.loads(json_string)
@@ -78,3 +79,72 @@ def async_detect_document(gcs_source_uri, gcs_destination_uri="gs://tohacks-buc/
 # gcs_source_uri = "claimletter-converted.pdf"
 # gcs_destination_uri = "gs://tohacks-buc/pdf_results/"
 # print(async_detect_document(gcs_source_uri))
+
+def upload_blob(source_file_name, bucket_name="tohacks-buc", destination_blob_name=""):
+    """Uploads a file to the bucket."""
+    # The ID of your GCS bucket
+    # bucket_name = "your-bucket-name"
+    # The path to your file to upload
+    # source_file_name = "local/path/to/file"
+    # The ID of your GCS object
+    # destination_blob_name = "storage-object-name"
+
+    destination_blob_name = os.path.split(source_file_name)[1]
+
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+
+    blob.upload_from_filename(source_file_name)
+
+    print(
+        "File {} uploaded to {}.".format(
+            source_file_name, destination_blob_name
+        )
+    )
+
+    # create pdf to text already
+    async_detect_document(destination_blob_name)
+
+    return "OK"
+
+# bucket_name = "tohacks-buc"
+# source_file_name = "/home/niyon/Downloads/claim-letter-05-converted.pdf"
+# upload_blob(source_file_name)
+
+def get_pdf_text(source_blob_name, bucket_name="tohacks-buc"):
+    """Downloads a blob from the bucket."""
+    # bucket_name = "your-bucket-name"
+    # source_blob_name = "storage-object-name"
+    # destination_file_name = "local/path/to/file"
+
+    source_blob_name = "pdf_results/"+ source_blob_name + "output-1-to-1.json"
+
+    storage_client = storage.Client()
+
+    bucket = storage_client.bucket(bucket_name)
+
+    # Construct a client side representation of a blob.
+    # Note `Bucket.blob` differs from `Bucket.get_blob` as it doesn't retrieve
+    # any content from Google Cloud Storage. As we don't need additional data,
+    # using `Bucket.blob` is preferred here.
+    # blob = bucket.blob(source_blob_name)
+    # blob.download_to_filename(destination_file_name)
+
+    blob = bucket.get_blob(source_blob_name).download_as_string()
+
+    response = json.loads(blob)
+
+    # The actual response for the first page of the input file.
+    first_page_response = response['responses'][0]
+    annotation = first_page_response['fullTextAnnotation']
+
+    return annotation['text']
+
+    # print(
+    #     "Blob {} downloaded to {}.".format(
+    #         source_blob_name, destination_file_name
+    #     )
+    # )
+
+# print(get_pdf_text("pdf_results/claimletter-converted.pdfoutput-1-to-1.json"))
