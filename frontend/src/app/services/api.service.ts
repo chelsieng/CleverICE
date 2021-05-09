dbad32fbb2f7037d98b1cd53962a59fe0e8863aa
chelsieng <chelsiengmk@gmail.com>
2021-05-09 04:43:19 -0400

    api service

---
+++ frontend/src/app/services/api.service.ts
@@ -0,0 +1,27 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+class Observable<T> {
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  private http: HttpClient;
+
+  constructor(http: HttpClient) {
+    this.http = http;
+  }
+
+  // getRequestName(argumentName?: typedef): Observable<returnTypeDef> {
+  //   let url = '/xxxxx/';
+  //   // some operations here
+  //   return this.http.get<returnTypeDef>(url);
+  // }
+  //
+  // postRequestName(argumentName: typedef): Observable<returnTypeDef> {
+  //   const url = '/xxxxx';
+  //   return this.http.post<returnTypeDef>(url, argumentName);
+  // }
+}
